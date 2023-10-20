34113451f3082903f3035b0b65db92391329ef9e
Lyubawa <lyubawa81@ukr.net>
2023-10-20 22:50:13 +0300

    hw-js-2

---
+++ task-1.js
@@ -0,0 +1,16 @@
+"use stict"
+
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    let order = quantity * pricePerDroid;
+    if (order > customerCredits) {
+        return `Insufficient funds!`;
+    } else {
+        return `You ordered ${quantity} droids worth ${order} credits!`;
+    }
+    }
+
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
